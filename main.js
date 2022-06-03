573c228823b1577ac73770f636233e444f6baf90
PraneethJain <praneethjain005@gmail.com>
2022-06-03 20:55:23 +0530

    Added hovering

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ let N = 16;
 
 for (let i=0; i<N; i++) {
     const row = document.createElement('div')
-    row.classList.add('row')
+    row.classList.add('row');
     for (let j=0; j<N;j++) {
         const cell = document.createElement('div');
-        cell.classList.add('cell')
-        row.appendChild(cell)
+        cell.classList.add('cell');
+        cell.addEventListener("mouseenter", e => {
+            e.target.classList.add('active');
+        });
+        cell.addEventListener("mouseleave", e=> {
+            e.target.classList.remove("active")
+        })
+        row.appendChild(cell);
     }
-    grid.appendChild(row)
+    grid.appendChild(row);
 }
