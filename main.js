e7ad4180c19b4e8bcc1b1f042f5bed6d493c7431
PraneethJain <praneethjain005@gmail.com>
2022-06-03 21:36:17 +0530

    Added clear button

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 const grid = document.querySelector('.grid');
-const sizeButton = document.querySelector('.size')
+const sizeButton = document.querySelector('#size')
+const clearButton = document.querySelector('#clear')
 
+window.N = 10;
 sizeButton.addEventListener('click', e => {
-    const N = parseInt(prompt("Creating an n by n grid. Enter the value of n"))
-    generateNewGrid(N)
+    let input = parseInt(prompt("Creating an n by n grid. Enter the value of n"));
+    if (input > 100 || input < 0) {
+        alert("Please give a value of N 1 and 100 (both included)");
+    } else {
+        window.N = input;
+    }
+    generateNewGrid(window.N);
 })
 
-let color = "#0000ff"
-generateNewGrid(16)
-function generateNewGrid(N) {
+clearButton.addEventListener('click', e => {
+    generateNewGrid(window.N);
+})
+
+let color = "#0000ff";
+generateNewGrid(window.N);
+function generateNewGrid(n) {
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild)
     }
-    for (let i=0; i<N; i++) {
+    for (let i=0; i<n; i++) {
         const row = document.createElement('div')
         row.classList.add('row');
-        for (let j=0; j<N;j++) {
+        for (let j=0; j<n;j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             cell.addEventListener("mouseenter", e => {
